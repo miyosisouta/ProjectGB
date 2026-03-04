8ea7ee770ebc8cd0315cf352a03a7bb9370e5a3e
永見凜 <jijimomo0818@docomo.ne.jp>
2026-03-04 17:58:29 +0900

    シーン遷移のクラスを変更

---
+++ GameTemplate/Game/src/Scene/OutGameScene.cpp
@@ -0,0 +1,40 @@
+﻿/**
+ * OutGameScene.cpp
+ *
+ * アウトゲームシーンの遷移
+ */
+
+#include "stdafx.h"
+#include "OutGameScene.h"
+#include "src/Scene/InGameScene.h"
+
+
+OutGameScene::OutGameScene()
+{
+
+}
+
+
+OutGameScene::~OutGameScene()
+{
+
+}
+
+
+bool OutGameScene::Start()
+{
+	return true;
+}
+
+
+void OutGameScene::Update()
+{
+
+}
+
+
+bool OutGameScene::RequestScene(uint32_t& id)
+{
+	//todo for test
+	if(g_pad[0]->IsTrigger(enButtonA))	
+}
---
+++ GameTemplate/Game/src/Scene/OutGameScene.h
@@ -0,0 +1,23 @@
+﻿/**
+ * OutGameScene.h
+ *
+ * アウトゲームシーンの遷移
+ */
+
+#pragma once
+#include "src/Scene/IScene.h"
+
+
+class OutGameScene : public IScene
+{
+public:
+	OutGameScene();
+	~OutGameScene();
+
+
+public:
+	bool Start() override;
+	void Update() override;
+	bool RequestScene(uint32_t& id) override;
+};
+
