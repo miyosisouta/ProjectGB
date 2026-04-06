27fd93cd80399fbebf0068b1e70eb20c6f2b07f1
Shimooka Minami <CA01244013@st.kawahara.ac.jp>
2026-04-06 20:12:36 +0900

    プレイヤー被ダメ時に顔が変わる

--- GameTemplate/Game/src/UI/InGameMenu.h
+++ GameTemplate/Game/src/UI/InGameMenu.h
@@ -11,9 +11,14 @@ class InGameMenu : public MenuBase
 private:
 	std::unique_ptr<UIAnimationSequence> abilitySkillIconScaleSequence_ = nullptr;
 	std::unique_ptr<UIAnimationSequence> bossHitHPPositionSequence_ = nullptr;
+	std::unique_ptr<TaskSchedulerSystem> playerDamageScheduler_ = nullptr;
 
 public:
 	void Update() override;
 	void Render(RenderContext& rc) override;
 	void InitializeLogic() override;
+
+
+private:
+	void CreatePlayerDamageScheduler();
 };
